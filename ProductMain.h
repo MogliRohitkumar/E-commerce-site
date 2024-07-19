b65e20808de31bfa17270246ca1080a227514ec8
Mogli Rohit Kumar <98601073+MogliRohitkumar@users.noreply.github.com>
2024-07-19 05:59:08 +0530

    Add files via upload

---
+++ ProductMain.h
@@ -0,0 +1,22 @@
+#ifndef PRODUCTMAIN_H
+#define PRODUCTMAIN_H
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class ProductMain{
+    private:
+        string Name,ProductID,response_id;
+        int Quantity;
+        float Price;
+    public:
+        ProductMain(string response_id,string Name,string ProductID,int Quantity,float Price); //Constructor
+        //Getters
+        string getName();
+        string getProductID();
+        float getPrice();
+        int getQuantity();
+        string getresponse_id();
+};
+#endif
+
---
+++ Sort.h
@@ -0,0 +1,16 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#include "ProductMain.h"
+
+class Sort
+{
+    public:
+        static bool compare_price(ProductMain ,ProductMain);
+        static bool compare_Name(ProductMain, ProductMain);
+};
+
+#endif
